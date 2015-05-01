7e1828ed9e7b175e7af4f59857c0445e0fc27898
Yakovenko Denis <yakovenko.denis.a@gmail.com>
2015-05-02 00:07:22 +0300

    Added slugs to books

--- Gemfile
+++ Gemfile
@@ -58,3 +58,5 @@ gem "paperclip", :git => "git://github.com/thoughtbot/paperclip.git"
 gem 'dotenv-rails', groups: :development
 
 gem 'aws-sdk', '<2.0'
+
+gem 'friendly_id'
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,18 +1,10 @@
 class Book < ActiveRecord::Base
+	extend FriendlyId
+	friendly_id :name, use: :slugged
+
 	belongs_to :user
 	has_attached_file :image
 	has_attached_file :resource
-
-	# validates_attachment_content_type :image,
-	# content_type: /\Aimage\/.*Z/,
-	# message: "Only images allowed"
-
-	# validates_attachment_content_type :resource,
-	# content_type: ['application/pdf'],
-	# message: "Only pdf files allowed"
-
-	# validates :image, attachment_presence: true
-	# validates :resource, attachment_presence: true
 
 	validates_attachment_content_type :image,
 	content_type:  /^image\/(png|gif|jpeg)/,
