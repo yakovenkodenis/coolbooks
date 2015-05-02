a561822e3d3b423e0e5f520600bfa6e3e586d38d
Yakovenko Denis <yakovenko.denis.a@gmail.com>
2015-05-02 11:52:40 +0300

    Added sale model

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,6 +3,7 @@ class Book < ActiveRecord::Base
 	friendly_id :name, use: :slugged
 
 	belongs_to :user
+	has_many :sales
 	has_attached_file :image
 	has_attached_file :resource
 
@@ -12,8 +13,12 @@ class Book < ActiveRecord::Base
 
 	validates_attachment_content_type :resource,
 	content_type: ['application/pdf'],
-	message: "Only pdfs allowed"
+	message: "Only pdf files allowed"
 
 	validates :image, attachment_presence: true
 	validates :resource, attachment_presence: true
+
+	validates_numericality_of :price,
+	greater_than: 49,
+	message: "Must be at least 50 cents"
 end
